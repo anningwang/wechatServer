bb6f3d4fa5d291ab7435125b9881872712aafcbe
anningwang <wxg001@21cn.com>
2018-08-24 18:56:04 +0800

    运行后创建菜单。

--- wechat/__init__.py
+++ wechat/__init__.py
@@ -2,8 +2,9 @@
 #  filename: __init__.py
 
 from apscheduler.schedulers.background import BackgroundScheduler
-import basic
 import threading
+import basic
+import menu
 
 
 __q = threading.Lock()        # create a lock object
@@ -23,5 +24,47 @@ scheduler.start()
 
 def get_access_token_for_wx():
     __q.acquire()
-    __token.get_access_token()
+    token = __token.get_access_token()
     __q.release()
+    return token
+
+
+menu_json = """
+    {
+        "button":
+        [
+            {
+                "type": "click",
+                "name": "开发指引",
+                "key":  "mpGuide"
+            },
+            {
+                "name": "公众平台",
+                "sub_button":
+                [
+                    {
+                        "type": "view",
+                        "name": "更新公告",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
+                    },
+                    {
+                        "type": "view",
+                        "name": "接口权限说明",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
+                    },
+                    {
+                        "type": "view",
+                        "name": "返回码说明",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1433747234&token=&lang=zh_CN"
+                    }
+                ]
+            },
+            {
+                "type": "click",
+                "name": "测试",
+                "key": "mp"
+            }
+          ]
+    }
+    """
+menu.Menu.create(menu_json, get_access_token_for_wx())      # 创建微信公众号菜单
--- wechat/menu.py
+++ wechat/menu.py
@@ -1,76 +1,36 @@
 # -*- coding: utf-8 -*-
 # filename: menu.py
 import urllib
-from wechat import get_access_token_for_wx
 
 
 class Menu(object):
     def __init__(self):
         pass
 
-    def create(self, post_data, access_token):
+    @staticmethod
+    def create(post_data, access_token):
         post_url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % access_token
         if isinstance(post_data, unicode):
             post_data = post_data.encode('utf-8')
         url_resp = urllib.urlopen(url=post_url, data=post_data)
         print url_resp.read()
 
-    def query(self, access_token):
+    @staticmethod
+    def query(access_token):
         post_url = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=%s" % access_token
         url_resp = urllib.urlopen(url=post_url)
         print url_resp.read()
 
-    def delete(self, access_token):
+    @staticmethod
+    def delete(access_token):
         post_url = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=%s" % access_token
         url_resp = urllib.urlopen(url=post_url)
         print url_resp.read()
 
     # 获取自定义菜单配置接口
-    def get_current_selfmenu_info(self, access_token):
+    @staticmethod
+    def get_current_self_menu_info(access_token):
         post_url = "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=%s" % access_token
         url_resp = urllib.urlopen(url=post_url)
         print url_resp.read()
 
-
-if __name__ == '__main__':
-    myMenu = Menu()
-    postJson = """
-    {
-        "button":
-        [
-            {
-                "type": "click",
-                "name": "开发指引",
-                "key":  "mpGuide"
-            },
-            {
-                "name": "公众平台",
-                "sub_button":
-                [
-                    {
-                        "type": "view",
-                        "name": "更新公告",
-                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
-                    },
-                    {
-                        "type": "view",
-                        "name": "接口权限说明",
-                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
-                    },
-                    {
-                        "type": "view",
-                        "name": "返回码说明",
-                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1433747234&token=&lang=zh_CN"
-                    }
-                ]
-            },
-            {
-                "type": "click",
-                "name": "旅行",
-                "key": "mp"
-            }
-          ]
-    }
-    """
-    my_access_token = get_access_token_for_wx()
-    myMenu.create(postJson, my_access_token)
